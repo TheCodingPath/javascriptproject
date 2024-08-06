a12648b084a9526a7def4c57a92fc7a62dcdaabe
TheCodingPath <studio@thecodingpath.com>
2024-08-06 13:08:52 +0500

    Update script.js

--- how to filter data in an html table using javascript/js/script.js
+++ how to filter data in an html table using javascript/js/script.js
@@ -30,32 +30,6 @@ let displayData = () => {
 
 displayData();
 
-let filterByPrice = () => {
-    getData().then(result => {
-        let rows = '';
-        let tbody = document.getElementById('myTable');
-        let val = document.getElementById('productPrice').value;
-        let prod = result.products;
-        //console.log(prod, val);
-        for (let i = 0; i < prod.length; i++) {
-            if (prod[i].price === val) {
-                console.log(prod, val);
-                rows += `<tr><td><img src="${prod[i].thumbnail}" width="200px" height="150px" alt="img"/></td>
-                    <td>${prod[i].title}</td>
-                    <td>${prod[i].brand}</td>
-                    <td>${prod[i].category}</td>
-                    <td>${prod[i].description}</td>
-                    <td>${prod[i].price}</td></tr>`
-
-                tbody.innerHTML = rows;
-            } else {
-                tbody.innerHTML = `<h3 style="color:red; margin-left:20px;">No record found for this price</h3>`;
-            }
-        }
-    })
-
-
-}
 let filterByCategory = () => {
     getData().then(result => {
         let rows = '';
